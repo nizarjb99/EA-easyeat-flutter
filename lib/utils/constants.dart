71f8127481cd472f44bb235d6422f795b0c1149a
manelcolominas <183344343+manelcolominas@users.noreply.github.com>
2026-05-11 16:52:05 +0200

    url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // Aqui van todas las constantes que no cambian
 class AppConstants {
 
-  // static const String baseUrl = 'http://localhost:1337';
+  //static const String baseUrl = 'http://localhost:1337';
 
   static const String baseUrl = 'https://ea-easyeat-backend2.onrender.com';
 
